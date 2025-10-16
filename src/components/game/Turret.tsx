d44857ebf41eaaa07fc8cb635f17591426b9e06c
Gabrielle Dandy Ramos [C] <gabrielle.ramos@grabtaxi.com>
2025-10-16 09:16:33 +0000

    change turrt design please, also when gaining shield the text in hud is

--- src/components/game/Turret.tsx
+++ src/components/game/Turret.tsx
@@ -1,45 +1,47 @@
-"use client";
 
-import { cn } from "@/lib/utils";
+"use client";
 
 export default function Turret() {
   return (
-    <div className="absolute bottom-0 left-1/2 -translate-x-1/2 w-48 h-24 pointer-events-none">
-      <svg viewBox="0 0 192 96" fill="none" xmlns="http://www.w3.org/2000/svg" className="w-full h-full">
+    <div className="w-40 h-20 pointer-events-none">
+      <svg viewBox="0 0 160 80" fill="none" xmlns="http://www.w3.org/2000/svg" className="w-full h-full">
         <defs>
-          <filter id="glow" x="-50%" y="-50%" width="200%" height="200%">
-            <feGaussianBlur stdDeviation="3.5" result="coloredBlur"/>
+          <filter id="turret-glow" x="-50%" y="-50%" width="200%" height="200%">
+            <feGaussianBlur stdDeviation="4" result="coloredBlur"/>
             <feMerge>
               <feMergeNode in="coloredBlur"/>
               <feMergeNode in="SourceGraphic"/>
             </feMerge>
           </filter>
         </defs>
-        {/* Turret Cannon */}
-        <path 
-          d="M86 20 H106 V60 H86 Z" 
-          className="fill-primary/30 stroke-primary stroke-2"
-          style={{ filter: "url(#glow)" }}
-        />
-        <path 
-          d="M88 20 H104 V58 H88 Z" 
-          className="fill-background"
-        />
-        <rect x="94" y="10" width="4" height="10" className="fill-primary" />
         
-        {/* Turret Base */}
+        {/* Cannon */}
         <path 
-          d="M60 90 C70 70, 122 70, 132 90 H60 Z"
-          className="fill-primary/30 stroke-primary stroke-2"
-          style={{ filter: "url(#glow)" }}
+          d="M72 10 L88 10 L92 50 L68 50 Z" 
+          className="fill-primary/20 stroke-primary stroke-1"
+          style={{ filter: "url(#turret-glow)" }}
         />
-         <path 
-          d="M65 90 C75 75, 117 75, 127 90 H65 Z"
+        <path 
+          d="M74 12 L86 12 L89 48 L71 48 Z" 
           className="fill-background"
         />
-        <circle cx="96" cy="80" r="10" className="fill-primary" />
-        <circle cx="96" cy="80" r="6" className="fill-background" />
+        <rect x="78" y="0" width="4" height="10" className="fill-primary" />
+        
+        {/* Base */}
+        <path 
+          d="M40 75 C40 65, 120 65, 120 75 L110 80 L50 80 Z"
+          className="fill-primary/30 stroke-primary stroke-1"
+          style={{ filter: "url(#turret-glow)" }}
+        />
+        <path 
+          d="M45 75 C45 68, 115 68, 115 75 L108 79 L52 79 Z"
+          className="fill-background"
+        />
 
+        {/* Central glowing orb */}
+        <circle cx="80" cy="65" r="8" className="fill-primary" />
+        <circle cx="80" cy="65" r="5" className="fill-background" />
+        <circle cx="80" cy="65" r="12" className="fill-primary/30" style={{ filter: "url(#turret-glow)" }} />
       </svg>
     </div>
   );
